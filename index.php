e148efd4e87f3a16e372cc21d192acd01f18cac4
MusanaKhem <hermann.acm@gmail.com>
2023-02-23 16:24:28 +0100

    blog project using PHP and composer and php library such as Ramsey uuid

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Based on uuid package's documentation - edit php file
+
+// Indicate which package is used and generate a random id
+use Ramsey\Uuid\Uuid;
+
+// Include name space
+// As we see, reference is missing (developer must use 'autoloader')
+$uuid = Uuid::uuid4();
+
+// Call function
+echo $uuid->toString() . PHP_EOL;
+
+?>
