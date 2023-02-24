f6a6ac4ffa682dca6dfd9e5cc0d0af13059f2ca5
MusanaKhem <hermann.acm@gmail.com>
2023-02-24 19:02:24 +0100

    Create article manager system and file to display articles

--- models/ArticleManager.php
+++ models/ArticleManager.php
@@ -1,4 +1,13 @@
 <?php
 
+    class ArticleManager extends Model{
+        // Create the function which is going to recover all articles in BDD
+        public function getArticles(){
+            return $this->getAll('articles', 'Article');
+
+            
+        }
+
+    }
 
 ?>
