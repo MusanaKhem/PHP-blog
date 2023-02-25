e6903d0254a2a8181a00b6edb521e1c1ef141e91
MusanaKhem <hermann.acm@gmail.com>
2023-02-25 11:51:28 +0100

    home controller system created

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,4 +1,34 @@
 <?php
 
+class HomeController {
+
+    // Create two private variable
+    private $_articleManager;
+    private $_view;
+
+    // 
+    public function __construct($url){
+
+        // 
+        if(isset($url) && count($url) > 1){
+
+            // 
+            throw new \Exception("Page was not found", 1);
+
+        }else{
+            $this->articles();
+        }
+    }
+
+    // 
+    private function articles(){
+
+        // 
+        $this->_articleManager = new ArticleManager();
+        $articles = $this->articleManager->getArticles();
+        require_once('homeView.php');
+
+    }
+}
 
 ?>
